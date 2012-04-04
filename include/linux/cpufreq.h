174ecc7896088c454fa6e134f2401130891fda8e
HomuHomu <ecoco666@gmail.com>
2012-04-04 20:48:14 +0900

    Add Sakuractive Gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -384,6 +384,9 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVE)
 extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lulzactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAKURACTIVE)
+extern struct cpufreq_governor cpufreq_gov_sakuractive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sakuractive)
 #endif
 
 
