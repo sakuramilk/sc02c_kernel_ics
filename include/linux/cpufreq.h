8f234e11124a33ab9972a4f443d48e2c3271189e
HomuHomu <ecoco666@gmail.com>
2012-04-04 21:09:24 +0900

    Add Wheatley Gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -387,6 +387,9 @@ extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SAKURACTIVE)
 extern struct cpufreq_governor cpufreq_gov_sakuractive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sakuractive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_wheatley)
 #endif
 
 
