989a38f962f7a0388678c8809991956745e557c1
HomuHomu <ecoco666@gmail.com>
2012-04-04 22:00:29 +0900

    Add Lazy Gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -396,6 +396,9 @@ extern struct cpufreq_governor cpufreq_gov_lagfree;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAZY)
+extern struct cpufreq_governor cpufreq_gov_lazy;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lazy)
 #endif
 
 
