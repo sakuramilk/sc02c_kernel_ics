ef3622912675f18db449987cc33a7ae4b1774c71
HomuHomu <ecoco666@gmail.com>
2012-04-05 22:20:03 +0900

    Add Smartass Gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -417,6 +417,9 @@ extern struct cpufreq_governor cpufreq_gov_brazilianwax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
 extern struct cpufreq_governor cpufreq_gov_interactiveX;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactiveX)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS)
+extern struct cpufreq_governor cpufreq_gov_smartass;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass)
 #endif
 
 
