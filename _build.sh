cd4a156b2b97613d2597bb65e979ee77afa17926
sakuramilk <c.sakuramilk@gmail.com>
2012-05-13 11:50:51 +0900

    update build script

--- _build.sh
+++ _build.sh
@@ -7,8 +7,8 @@ INITRAMFS_TMP_DIR=/tmp/sc02c_initramfs
 
 cpoy_initramfs()
 {
-  if [ -d /tmp/sc02c_initramfs ]; then
-    rm -rf /tmp/sc02c_initramfs  
+  if [ -d $INITRAMFS_TMP_DIR ]; then
+    rm -rf $INITRAMFS_TMP_DIR  
   fi
   cp -a $INITRAMFS_SRC_DIR $(dirname $INITRAMFS_TMP_DIR)
   rm -rf $INITRAMFS_TMP_DIR/.git
@@ -24,8 +24,10 @@ case "$BUILD_TARGET" in
   "MULTI" ) BUILD_DEFCONFIG=u1_sc02c_multi_defconfig ;;
   * ) echo "error: not found BUILD_TARGET" && exit -1 ;;
 esac
-OUTPUT_DIR=out/$BUILD_TARGET
-
+BIN_DIR=out/$BUILD_TARGET/bin
+OBJ_DIR=out/$BUILD_TARGET/obj
+mkdir -p $BIN_DIR
+mkdir -p $OBJ_DIR
 
 # generate boot splash header
 if [ ! -n "$3" ]; then
@@ -84,8 +86,8 @@ if [ "$BUILD_SELECT" = 'all' -o "$BUILD_SELECT" = 'a' ]; then
   echo ""
   echo "=====> cleaning"
   make clean
-  cp -f ./arch/arm/configs/$BUILD_DEFCONFIG ./.config
-  make -C $PWD oldconfig || exit -1
+  cp -f ./arch/arm/configs/$BUILD_DEFCONFIG $OBJ_DIR/.config
+  make -C $PWD O=$OBJ_DIR oldconfig || exit -1
 fi
 
 if [ "$BUILD_SELECT" != 'zImage' -a "$BUILD_SELECT" != 'z' ]; then
@@ -94,7 +96,7 @@ if [ "$BUILD_SELECT" != 'zImage' -a "$BUILD_SELECT" != 'z' ]; then
   if [ -e make.log ]; then
     mv make.log make_old.log
   fi
-  nice -n 10 make -j12 2>&1 | tee make.log
+  nice -n 10 make O=$OBJ_DIR -j12 2>&1 | tee make.log
 fi
 
 # check compile error
@@ -112,7 +114,7 @@ find -name '*.ko' -exec cp -av {} $INITRAMFS_TMP_DIR/lib/modules/ \;
 # build zImage
 echo ""
 echo "=====> make zImage"
-nice -n 10 make -j2 zImage CONFIG_INITRAMFS_SOURCE="$INITRAMFS_TMP_DIR" CONFIG_INITRAMFS_ROOT_UID=`id -u` CONFIG_INITRAMFS_ROOT_GID=`id -g` || exit 1
+nice -n 10 make O=$OBJ_DIR -j2 zImage CONFIG_INITRAMFS_SOURCE="$INITRAMFS_TMP_DIR" CONFIG_INITRAMFS_ROOT_UID=`id -u` CONFIG_INITRAMFS_ROOT_GID=`id -g` || exit 1
 
 if [ ! -e $OUTPUT_DIR ]; then
   mkdir -p $OUTPUT_DIR
@@ -121,43 +123,43 @@ fi
 echo ""
 echo "=====> CREATE RELEASE IMAGE"
 # clean release dir
-if [ `find ./$OUTPUT_DIR -type f | wc -l` -gt 0 ]; then
-  rm $KERNEL_DIR/$OUTPUT_DIR/*
+if [ `find $BIN_DIR -type f | wc -l` -gt 0 ]; then
+  rm $BIN_DIR/*
 fi
 
 # copy zImage
-cp arch/arm/boot/zImage ./$OUTPUT_DIR/
-cp arch/arm/boot/zImage ./out/
-echo "  out/$OUTPUT_DIR/zImage"
+cp $OBJ_DIR/arch/arm/boot/zImage $BIN_DIR/zImage
+cp $OBJ_DIR/arch/arm/boot/zImage ./out/
+echo "  $BIN_DIR/zImage"
 echo "  out/zImage"
 
 # create odin image
-cd $KERNEL_DIR/$OUTPUT_DIR
+cd $KERNEL_DIR/$BIN_DIR
 tar cf $BUILD_LOCALVERSION-odin.tar zImage
 md5sum -t $BUILD_LOCALVERSION-odin.tar >> $BUILD_LOCALVERSION-odin.tar
 mv $BUILD_LOCALVERSION-odin.tar $BUILD_LOCALVERSION-odin.tar.md5
-echo "  $OUTPUT_DIR/$BUILD_LOCALVERSION-odin.tar.md5"
+echo "  $BIN_DIR/$BUILD_LOCALVERSION-odin.tar.md5"
 
 # create cwm image
-cd $KERNEL_DIR/$OUTPUT_DIR
+cd $KERNEL_DIR/$BIN_DIR
 if [ -d tmp ]; then
   rm -rf tmp
 fi
-mkdir -p tmp/META-INF/com/google/android
+mkdir -p ./tmp/META-INF/com/google/android
 cp zImage ./tmp/
-cp $KERNEL_DIR/release-tools/update-binary $KERNEL_DIR/$OUTPUT_DIR/tmp/META-INF/com/google/android/
-sed -e "s/@VERSION/$BUILD_LOCALVERSION/g" $KERNEL_DIR/release-tools/updater-script.sed > $KERNEL_DIR/$OUTPUT_DIR/tmp/META-INF/com/google/android/updater-script
+cp $KERNEL_DIR/release-tools/update-binary ./tmp/META-INF/com/google/android/
+sed -e "s/@VERSION/$BUILD_LOCALVERSION/g" $KERNEL_DIR/release-tools/updater-script.sed > ./tmp/META-INF/com/google/android/updater-script
 cd tmp && zip -rq ../cwm.zip ./* && cd ../
 SIGNAPK_DIR=$KERNEL_DIR/release-tools/signapk
 java -jar $SIGNAPK_DIR/signapk.jar $SIGNAPK_DIR/testkey.x509.pem $SIGNAPK_DIR/testkey.pk8 cwm.zip $BUILD_LOCALVERSION-signed.zip
 rm cwm.zip
 rm -rf tmp
-echo "  $OUTPUT_DIR/$BUILD_LOCALVERSION-signed.zip"
+echo "  $BIN_DIR/$BUILD_LOCALVERSION-signed.zip"
 
 # rename zImage for multiboot
 if [ "$BUILD_TARGET" = "MULTI" ]; then
-    echo "  rename $OUTPUT_DIR/zImage => $OUTPUT_DIR/zImage_ics"
-    cp zImage zImage_ics
+    echo "  rename $BIN_DIR/zImage => $BIN_DIR/zImage_ics"
+    cp $BIN_DIR/zImage $BIN_DIR/zImage_ics
 fi
 
 cd $KERNEL_DIR
